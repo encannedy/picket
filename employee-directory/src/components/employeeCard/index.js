3cdcf976ed6222399ba471976f1cf53852a54b3e
Erica Cannedy <encannedy@gmail.com>
2020-07-26 22:06:28 -0400

    initial react app created

---
+++ employee-directory/src/components/employeeCard/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./style.css";
+
+function EmployeeCard(props) {
+  return (
+    <div className="card">
+      <div className="img-container">
+        <img alt={props.name} src={props.picture} />
+      </div>
+      <div className="content">
+        <ul>
+          <li>
+            <strong>Name:</strong> {props.name}
+          </li>
+          <li>
+            <strong>Email:</strong> {props.email}
+          </li>
+          <li>
+            <strong>Phone Number:</strong> {props.cell}
+          </li>
+
+        </ul>
+      </div>
+      <span onClick={() => props.removeEmployee(props.id)} className="remove">
+        𝘅
+      </span>
+    </div>
+  );
+}
+
+export default EmployeeCard;
